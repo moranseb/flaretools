87cda5834a2ba78f90fe7668e97c18b5241f1276
Seb Moran <moran.seb@protonmail.com>
2023-02-02 19:31:37 -0600

    Moved api_calls_v2.py to main, minor updates on flaretools.py

--- flaretools.py
+++ flaretools.py
@@ -1,11 +1,9 @@
-import argparse
 import csv
 import json
 import requests
 import sys
 
 import program_strings
-import api_calls_v2
 import config
 
 
@@ -13,8 +11,11 @@ def main():
 
     print(program_strings.logo)
     print("\n\n\nWell then, here we go... Exciting...")
-    APIfalures = 3  # Change how many API falures before the program exits.
 
+    global APIfalures
+    APIfalures = 50  # Change how many API falures before the program exits.
+
+    global issue_log
     issue_log = []
     csv_file = sys.argv[1]
 
@@ -53,14 +54,152 @@ def _make_api_calls(row, headers):
     account_id = config.account_id
     IPv4 = config.IPv4
 
-    add_zone_response = api_calls_v2.create_zone(headers, domain_name, account_id)
-    zone_identifier = add_zone_response["result"]["id"]
+    add_zone_response = create_zone(headers, domain_name, account_id)
+    if add_zone_response["success"] == False:
+        pass
+    else:
+        zone_identifier = add_zone_response["result"]["id"]
 
-    O365_sender = row["O365 Sender"]
-    api_calls_v2.add_dns_rules(headers, zone_identifier, domain_name, IPv4, account_id, O365_sender)
+        O365_sender = row["0365 Sender"]
+        add_dns_rules(headers, zone_identifier, domain_name, IPv4, account_id, O365_sender)
 
-    website = row["Website"]
-    api_calls_v2.add_page_rules(headers, domain_name, website, zone_identifier)
+        website = row["Website "]
+        add_page_rules(headers, domain_name, website, zone_identifier)
+
+
+
+def create_zone(headers, domain_name, account_id):
+    parameters = {
+        "name": f"{domain_name}",
+        "account": {
+            "id": f"{account_id}",
+        },
+        "jump_start": "false",
+        "type": "full",
+    }
+
+    response = requests.post(
+        "https://api.cloudflare.com/client/v4/zones", headers=headers, json=parameters
+    )
+
+    response = json.loads(response.text)
+
+    if response["success"] == True:
+        print(
+            f"[*] API call to add domain {domain_name}, success: {response['success']}, errors: {response['errors']}"
+        )
+
+    if response["success"] == False:
+        print(f"\n[WARNING] API call to add domain {domain_name} failed.\n")
+        global APIfalures
+        APIfalures -= 1
+        issue_log.append(f"API call to add {domain_name} failed.")
+
+    return response
+
+def add_dns_rules(headers, zone_identifier, domain_name, IPv4, account_id, O365_sender):
+
+    for i in range(5):
+
+        url = f"https://api.cloudflare.com/client/v4/zones/{zone_identifier}/dns_records"
+
+        type_list= ("A", "CNAME", "CNAME", "CNAME", "TXT")
+        dns_type = type_list[i]
+
+        name_list= ("@", "www", "selector1._domainkey", "selector2._domainkey", "_dmarc")
+        name = name_list[i]
+
+        content_list = [f"{IPv4}", "@", f"selector1-{domain_name}-com._domainkey.abstraktmktg.onmicrosoft.com", 
+            f"selector2-{domain_name}-com._domainkey.abstraktmktg.onmicrosoft.com", f"v=DMARC1; p=none; pct=100; rua={O365_sender}; ruf=mailto:{O365_sender}; fo=1"]
+        content = content_list[i]
+
+        ttl_tuple = (1, 1, 120, 120, 120)
+        ttl= ttl_tuple[i]
+
+        proxied_tuple = (True, True, False, False)
+        if i == 4:
+            pass
+        else:
+            proxied = proxied_tuple[i]
+
+        if i == 4:
+            parameters = {
+                "type": dns_type,
+                "name": name,
+                "content": content,
+                "ttl": ttl
+            }
+
+        else:
+            parameters = {
+                "type": dns_type,
+                "name": name,
+                "content": content,
+                "ttl": ttl,
+                "proxied": proxied
+            }
+
+        response = requests.post(
+            url,
+            headers=headers,
+            json=parameters
+        )
+
+        response = json.loads(response.text)
+
+        if response["success"] == True:
+            print(
+                f"[*] API call to add DNS rule {str(i+1)} for {domain_name}, success: {response['success']}, errors: {response['errors']}"
+            )
+
+        if response["success"] == False:
+            print(f"\n[WARNING] API call to add DNS rule {str(i+1)} for {domain_name} failed.\n")
+            global APIfalures
+            APIfalures -= 1
+            issue_log.append(f"API call to add DNS {str(i+1)} rule for {domain_name} failed.")
+
+def add_page_rules(headers, domain_name, website, zone_identifier):
+
+    parameters = {
+        "targets": [
+            {
+                "target": "url",
+                "constraint": {
+                    "operator": "matches",
+                    "value": f"*{domain_name}",
+                },
+            },
+        ],
+        "actions": [
+            {
+                "id": "forwarding_url",
+                "value": {
+                    "url": f"http://{website}",
+                    "status_code": 301
+                },
+            },
+        ],
+        "status": "active",
+    }
+
+    response = requests.post(
+        f"https://api.cloudflare.com/client/v4/zones/{zone_identifier}/pagerules",
+        headers=headers,
+        json=parameters,
+    )
+
+    response = json.loads(response.text)
+
+    if response["success"] == True:
+        print(
+            f"[*] API call to add a page rule for {domain_name}, success: {response['success']}, errors: {response['errors']}"
+        )
+
+    if response["success"] == False:
+        print(f"\n[WARNING] API call to add a page rule for {domain_name} failed.\n")
+        global APIfalures
+        APIfalures -= 1
+        issue_log.append(f"API call to add a page rule for {domain_name} failed.")
 
 main()
-print("\n\n\n *** So long, and thanks for all the fish *** ")
+print("\n\n\n *** So long, and thanks for all the fish *** \n\n")
